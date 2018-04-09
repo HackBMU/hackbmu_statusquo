740e1d6212106d1e2283fecd07339d7a00b57314
Shubham Kachroo <shubhamkachroo@yahoo.co.in>
2018-04-09 14:43:50 +0530

    Delete firebase_testing.py

--- firebase_testing.py
+++
@@ -1,6 +0,0 @@
-from firebase import firebase
-
-firebase = firebase.FirebaseApplication('https://statusquo-3964e.firebaseio.com/', None)
-
-result = firebase.post('/accidents' ,{'level':'1'})
-print(result)
